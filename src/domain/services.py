75517a52df748528fac94cfa41a1ce71e09e4498
Aleksandr <gggols32@gmail.com>
2025-03-25 12:38:59 +0300

    Update Game Service

--- src/domain/services.py
+++ src/domain/services.py
@@ -1,15 +1,17 @@
-from .models import User, Action
-from .repositories import ScoreRepository, InventoryRepository, UserRepository, LLMRepository
+from .models import User, Action, Event
+from .repositories import ScoreRepository, InventoryRepository, UserRepository, EventRepository, LLMRepository
 
 class GameService:
     def __init__(self, 
                  user_repo: UserRepository,
                  inventory_repo: InventoryRepository,
                  score_repo: ScoreRepository,
+                 event_repo: EventRepository,
                  llm_repo: LLMRepository):
         self.user_repo = user_repo
         self.inventory_repo = inventory_repo
         self.score_repo = score_repo
+        self.event_repo = event_repo
         self.llm_repo = llm_repo
     
     def create_user(self, name: str, hashed_password: str) -> User:
@@ -36,6 +38,15 @@ class GameService:
 
     def make_action(self, user: User, action: str) -> Action:
         result = self.llm_repo.make_action(action)
-        self.inventory_repo.update_user_invetory(user, result["inventory"])
-        self.score_repo.update_user_score(user, result["score"])
+        self.inventory_repo.update_user_invetory(user, result.inventory)
+        self.score_repo.update_user_score(user, result.score)
         return result
+    
+    def write_event(self, user: User, description: str) -> None:
+        return self.event_repo.create_event(description, user)
+    
+    def get_user_events(self, user: User) -> list[Event]:
+        return self.event_repo.get_user_events(user)
+    
+    def get_all_events(self) -> list[Event]:
+        return self.event_repo.get_all_events()
